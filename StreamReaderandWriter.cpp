abd0d078a4864f5dddf44391e016e9e09e6d6a14
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-06-02 20:25:34 +0700

    menambahkan penanganan jika file tidak ada

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -48,4 +48,7 @@ int main()
          //menutup file jika telah selesai
         infile.close();
     }
+    //kondisi jika file tidak ada
+    else cout << "Unable to open file" ;
+    return 0;
 }

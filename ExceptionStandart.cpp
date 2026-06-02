1eeaa7262e5447ec84723820a3d3f1af828dda1e
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-06-02 19:57:34 +0700

    menambahkan blok try

--- ExceptionStandart.cpp
+++ ExceptionStandart.cpp
@@ -2,3 +2,14 @@
 #include <exception>
 #include <array>
 using namespace std;
+
+int main(){
+    cout << "Awal Program" << endl;
+    try 
+    {
+        //deklarasi array data dengan panjang index 5
+        array <int, 3> data = {1, 2, 3};
+        //memanggil array elemnt ke-5 yang berarti akan terjadi error
+        cout << data.at(5) << endl;
+    }
+}
